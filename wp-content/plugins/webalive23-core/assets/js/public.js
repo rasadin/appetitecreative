fe17160673f9f39867c0e66f593d5a412497104f
rasadin <12105708+rasadin@users.noreply.github.com>
2024-06-10 23:12:19 +0600

    plugin

--- wp-content/plugins/webalive23-core/assets/js/public.js
+++ wp-content/plugins/webalive23-core/assets/js/public.js
@@ -23,84 +23,84 @@
         });
     };
 
-    var boxAnimation2 = function ($scope, $) {
-        $(document).ready(function () {
-            var $box1 = $scope.find('.box1');
-            var $box3 = $scope.find('.box3');
+    // var boxAnimation2 = function ($scope, $) {
+    //     $(document).ready(function () {
+    //         var $box1 = $scope.find('.box1');
+    //         var $box3 = $scope.find('.box3');
 
-            var controller = new ScrollMagic.Controller();
+    //         var controller = new ScrollMagic.Controller();
 
-            var tween = gsap.timeline()
-                .fromTo($box1, {x: '10vw'}, {x: '-15vw', ease: 'none'}, 0)
-                .fromTo($box3, {x: '-10vw'}, {x: '15vw', ease: 'none'}, 0);
+    //         var tween = gsap.timeline()
+    //             .fromTo($box1, {x: '10vw'}, {x: '-15vw', ease: 'none'}, 0)
+    //             .fromTo($box3, {x: '-10vw'}, {x: '15vw', ease: 'none'}, 0);
 
-            new ScrollMagic.Scene({
-                triggerElement: $box3[0], // Use the first DOM element from the jQuery object
-                triggerHook: 0.9,
-                duration: '100%'
-            })
-                .setTween(tween)
-                .addTo(controller);
-        });
-    };
+    //         new ScrollMagic.Scene({
+    //             triggerElement: $box3[0], // Use the first DOM element from the jQuery object
+    //             triggerHook: 0.9,
+    //             duration: '100%'
+    //         })
+    //             .setTween(tween)
+    //             .addTo(controller);
+    //     });
+    // };
 
-    var textLoop = function ($scope, $) {
-        $(document).ready(function () {
-            var $scrollContent = $scope.find('.loop-content');
-            var $scrollItems = $scope.find('.loop-item');
-            var totalWidth = 0;
+    // var textLoop = function ($scope, $) {
+    //     $(document).ready(function () {
+    //         var $scrollContent = $scope.find('.loop-content');
+    //         var $scrollItems = $scope.find('.loop-item');
+    //         var totalWidth = 0;
 
-            // Calculate total width of all items
-            $scrollItems.each(function() {
-                totalWidth += $(this).outerWidth(true);
-            });
+    //         // Calculate total width of all items
+    //         $scrollItems.each(function() {
+    //             totalWidth += $(this).outerWidth(true);
+    //         });
 
-            // Duplicate the items for seamless scrolling
-            $scrollContent.append($scrollItems.clone());
+    //         // Duplicate the items for seamless scrolling
+    //         $scrollContent.append($scrollItems.clone());
 
-            // Set the width of the scroll-content
-            $scrollContent.width(totalWidth * 2);
+    //         // Set the width of the scroll-content
+    //         $scrollContent.width(totalWidth * 2);
 
-            // Adjust the animation duration based on total width
-            var animationDuration = totalWidth / 50; // Adjust this ratio as needed
+    //         // Adjust the animation duration based on total width
+    //         var animationDuration = totalWidth / 50; // Adjust this ratio as needed
 
-            // Initialize GSAP animation
-            gsap.to($scrollContent, {
-                x: -totalWidth,
-                duration: animationDuration,
-                ease: 'none',
-                repeat: -1
-            });
-        });
-    };
+    //         // Initialize GSAP animation
+    //         gsap.to($scrollContent, {
+    //             x: -totalWidth,
+    //             duration: animationDuration,
+    //             ease: 'none',
+    //             repeat: -1
+    //         });
+    //     });
+    // };
 
 
 
-    var gridAnimation = function ($scope, $) {
-        $(document).ready(function () {
-            // Ensure elements are scoped within $scope
-            var $box1 = $scope.find('.box1');
-            var $box2 = $scope.find('.box2');
-            var $box3 = $scope.find('.box3');
-            var $box4 = $scope.find('.box4');
+    // var gridAnimation = function ($scope, $) {
+    //     $(document).ready(function () {
+    //         // Ensure elements are scoped within $scope
+    //         var $box1 = $scope.find('.box1');
+    //         var $box2 = $scope.find('.box2');
+    //         var $box3 = $scope.find('.box3');
+    //         var $box4 = $scope.find('.box4');
 
-            var controller = new ScrollMagic.Controller();
+    //         var controller = new ScrollMagic.Controller();
 
-            var tween = gsap.timeline()
-                .fromTo($box1, { x: '2vw' }, { x: '-15vw', ease: 'none' }, 0)
-                .fromTo($box3, { x: '2vw' }, { x: '-10vw', ease: 'none' }, 0)
-                .fromTo($box2, { x: '-2vw' }, { x: '15vw', ease: 'none' }, 0)
-                .fromTo($box4, { x: '-2vw' }, { x: '10vw', ease: 'none' }, 0);
+    //         var tween = gsap.timeline()
+    //             .fromTo($box1, { x: '2vw' }, { x: '-15vw', ease: 'none' }, 0)
+    //             .fromTo($box3, { x: '2vw' }, { x: '-10vw', ease: 'none' }, 0)
+    //             .fromTo($box2, { x: '-2vw' }, { x: '15vw', ease: 'none' }, 0)
+    //             .fromTo($box4, { x: '-2vw' }, { x: '10vw', ease: 'none' }, 0);
 
-            new ScrollMagic.Scene({
-                triggerElement: $box3[0], // Use the third box as the trigger element
-                triggerHook: 0.9,
-                duration: '100%'
-            })
-                .setTween(tween)
-                .addTo(controller);
-        });
-    };
+    //         new ScrollMagic.Scene({
+    //             triggerElement: $box3[0], // Use the third box as the trigger element
+    //             triggerHook: 0.9,
+    //             duration: '100%'
+    //         })
+    //             .setTween(tween)
+    //             .addTo(controller);
+    //     });
+    // };
 
 
     $(window).on('elementor/frontend/init', function () {
